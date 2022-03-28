1bbb596822c2158e2e60e431830254366aa6ef87
dN <96394836+dNikolikj@users.noreply.github.com>
2022-03-28 15:42:21 +0200

    Add files via upload

---
+++ HomeworkClass 07/Domain/Models/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Circle : Shape
+    {
+        public double Radius { get; set; }
+
+        public override void GetPerimetar()
+        {
+            double result = Math.PI * Radius * 2;
+            Console.WriteLine($"The perimeter of the circle is {result}cm2.");
+        }
+
+        public override void GetArea()
+        {
+            double result = Math.PI * Radius * Radius;
+            Console.WriteLine($"The area of the circle is {result}cm2.");
+        }
+    }
+}
+
---
+++ HomeworkClass 07/Domain/Models/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Rectangle:Shape
+    {
+        public double SideA { get; set; }
+        public double SideB { get; set; }
+
+
+
+
+        public override void GetPerimetar()
+        {
+            double result = 2 * (SideA + SideB);
+            Console.WriteLine($"The perimeter of the rectangle is {result}cm2.");
+        }
+        public override void GetArea()
+        {
+           double result = SideA * SideB;
+            Console.WriteLine($"The area of the rectangle is {result}cm2.");
+        }
+
+       
+    }
+}
